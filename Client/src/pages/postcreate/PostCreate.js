4f33de3e8afa0ad15fc9e1fac81463eae67930de
benjhatch <benjamin.hat4@gmail.com>
2019-08-09 23:25:21 -0400

    using draft js

--- Client/src/pages/postcreate/PostCreate.js
+++ Client/src/pages/postcreate/PostCreate.js
@@ -1,22 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import {Editor, EditorState} from 'draft-js';
 
 function PostCreate() {
-    const [title, setTitle] = useState('');
-    const [height, setHeight] = useState("45px");
-
-    function handleChange(e) {
-        e.target.style.height = "45px";
-        setHeight(e.target.scrollHeight);
-        setTitle(e.target.value);
-    }
-
-    useEffect(() => {
-        document.title = title
-    }, [title])
+    const [editorState, setEditorState] = React.useState(
+        EditorState.createEmpty()
+    );
 
     return (
-        <div className="create-container">
-            <textarea style={{height: height}} className="create-title" placeholder="Title" value={title} onChange={handleChange} />
+        <div className="editor">
+            <Editor
+            editorState={editorState}
+            onChange={setEditorState}
+            />
         </div>
     );
 }
